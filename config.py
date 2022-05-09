5c63c887308e0c37f084e3886ff04a6b56d1020f
Dindihub <sadindi03@gmail.com>
2022-05-09 23:43:21 +0300

    COde-Add secret key from .env

--- config.py
+++ config.py
@@ -1,11 +1,20 @@
 import os
 from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv)
-
-
+load_dotenv(find_dotenv())
 class Config(object):
     SQLALCHEMY_DATABASE_URI=os.getenv('DATABASE_URL')
     DEBUG = os.getenv('DEBUG')
+    SECRET_KEY= os.getenv('SECRET_KEY')
 
-    breakpoint()
+class ProdConfig(Config):
+    pass
+
+
+class DevConfig(Config):
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+} 
