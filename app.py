0d0a606d03dcff9cb63e505d11e8d5e51cb99b3a
Dindihub <sadindi03@gmail.com>
2022-05-09 23:41:28 +0300

    Code-Add secret key to app to allow crsf

--- app.py
+++ app.py
@@ -1,13 +1,26 @@
+import os
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from .main import main as main_blueprint 
 
-app = Flask(__name__)
 
+db=SQLAlchemy()
 
-@app.route('/')
-def home():
-    return '<h1>test<h1>'
+from .main import models
+
+def create_app():
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'wordpass'
+    app.register_blueprint(main_blueprint)
+    app.config['SQLALCHEMY_DATABASE_URI']=os.getenv('DATABASE_URL')
+   
+    db.init_app(app)
+    migrate = Migrate(app,db)
+    
+    return app
 
 
 if __name__=='__main__':
-    app.run()
+    create_app()
 
