2c2253dae5480077e43157a1c90ae3f5f317c6be
Dindihub <sadindi03@gmail.com>
2022-05-09 23:48:57 +0300

    Code-Add routes for About,Home,Register and Login page

--- main/views.py
+++ main/views.py
@@ -1,8 +1,29 @@
-from flask import render_template
+from flask import render_template,url_for,redirect,flash
 from . import main
+from .forms import RegistrationForm,LoginForm
 
 
 
 @main.route('/')
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html',title='home')
+
+@main.route('/about')
+def about():
+    return render_template('about.html',title='about')
+
+
+@main.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template('register.html',title = 'register',form = form)
+
+@main.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html',title = 'login',form = form)
+
+
+
+
+
