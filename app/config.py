3bbb790e9bcab6149441b8026d9987903540867f
Dindihub <sadindi03@gmail.com>
2022-05-12 22:48:59 +0300

    7th deploy

--- app/config.py
+++ app/config.py
@@ -9,7 +9,7 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS=False
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
-    # SQLALCHEMY_DATABASE_URI='postgres://bnpegjulzxkznr:754192c1cc4cc0ddc69dcfef3800b75f6b550ec9b87821acee76c0cfd2b511dc@ec2-52-4-104-184.compute-1.amazonaws.com:5432/d966counsre75h'
+    SQLALCHEMY_DATABASE_URI='postgres://bnpegjulzxkznr:754192c1cc4cc0ddc69dcfef3800b75f6b550ec9b87821acee76c0cfd2b511dc@ec2-52-4-104-184.compute-1.amazonaws.com:5432/d966counsre75h'
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI='postgresql://bnpegjulzxkznr:754192c1cc4cc0ddc69dcfef3800b75f6b550ec9b87821acee76c0cfd2b511dc@ec2-52-4-104-184.compute-1.amazonaws.com:5432/d966counsre75h'
